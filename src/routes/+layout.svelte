578d0a92cbc98b15af535afb78a4e4ad64c97681
Dylan Berutti <beruttid@gmail.com>
2025-10-16 19:32:21 -0400

    trying my hand

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const name = "Dylan Berutti";
+    const asciiName = ``.trim();
+</script>
+
+<pre class="ascii-banner" aria-label="ascii-name">{asciiName}</pre>
+
+<header class="site-header">
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+    </nav>
+</header>
+
+<main class="site-content">
+    <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,3 @@
-<script lang="ts">
-    const asciiName = ``;
-</script>
-
-<pre class="ascii-banner" aria-label="ascii-name">{asciiName}</pre>
-
-<header class="site-header">
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/projects">Projects</a>
-    </nav>
-</header>
-
-<main class="site-content">
-    <slot />
-</main>
-
 <section class="starter" style="text-align: center; padding: 4rem 1rem;">
     <h1>Hi, I'm Dylan</h1>
     <p></p>
