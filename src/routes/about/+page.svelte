7fcdb0fafdb20920c96f60fc382e09a9d1386c19
Dylan Berutti <beruttid@gmail.com>
2025-10-17 11:21:08 -0400

    getting the website up

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<section style="padding: 2rem">
+    <h2>About Me!</h2>
+    <p>
+        Hi, I'm Dylan! I'm a sophomore CS major at the University of Pittsburgh
+        with a passion for low level systems and UNIX.
+    </p>
+</section>
