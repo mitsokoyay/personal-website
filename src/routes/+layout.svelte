c42f7480dcd259794bfe6cb77996c68afb65f25c
Dylan Berutti <beruttid@gmail.com>
2025-10-20 14:49:45 -0400

    removing things i dont want

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,13 @@
 </script>
 
 <pre class="ascii-banner" aria-label="ascii-name">{asciiName}</pre>
-
-<header class="site-header">
-    <nav class="nav">
-        <a href="/" class="home-link">Home</a>
-        <a href="/about">About</a>
-        <a href="/projects">Projects</a>
-    </nav>
-</header>
+<section class="home" style="text-align:center; padding: 2rem;">
+    <div class="navbar">
+        <a class="active" href="home">Home</a>
+        <a href="contact">Contact</a>
+        <a href="about">About</a>
+    </div>
+</section>
 
 <main class="site-content">
     <slot />
